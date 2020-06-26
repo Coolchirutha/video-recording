c46e08a04523bf28dccd1cc4bd7f9ea9d48165a0
Mouni Nischal <42888971+Coolchirutha@users.noreply.github.com>
2020-06-26 11:57:49 +0530

    Updates

---
+++ precache-manifest.2618e2b38a3d0deb3aa1cfb45ba45f84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3cf5a561d3de05ec28cb6e6b87771fb",
+    "url": "/video-recording/index.html"
+  },
+  {
+    "revision": "fd78a6bb420474a6660c",
+    "url": "/video-recording/static/css/main.7f2b0194.chunk.css"
+  },
+  {
+    "revision": "3fc5003fe555124e206d",
+    "url": "/video-recording/static/js/2.aa1b32d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/video-recording/static/js/2.aa1b32d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd78a6bb420474a6660c",
+    "url": "/video-recording/static/js/main.75d2b57e.chunk.js"
+  },
+  {
+    "revision": "c03eb7f1587305ce3a67",
+    "url": "/video-recording/static/js/runtime-main.fe5dafff.js"
+  }
+]);
