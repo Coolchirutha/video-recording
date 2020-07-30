f44d8e0e805630d5d0e706e0562170699f086ade
Mouni Nischal <d.chiranjeevi007@gmail.com>
2020-07-30 16:56:45 +0530

    Updates

--- precache-manifest.2cd8ea6a6e5b4e1020dc0a6ec7aec85d.js
+++ precache-manifest.2cd8ea6a6e5b4e1020dc0a6ec7aec85d.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9f5aa447180fc8b1023db5906b59bfcb",
+    "revision": "e46ccedf3a1a23c801356856160a9451",
     "url": "/video-recording/index.html"
   },
   {
-    "revision": "fdfae68ece04d7229dd6",
+    "revision": "ac2e369631f72cb76a8a",
     "url": "/video-recording/static/css/main.7f2b0194.chunk.css"
   },
   {
-    "revision": "24087d1083199f7056c3",
-    "url": "/video-recording/static/js/2.a09991e3.chunk.js"
+    "revision": "0f7b43c90c8a2417fcec",
+    "url": "/video-recording/static/js/2.5651cf6f.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/video-recording/static/js/2.a09991e3.chunk.js.LICENSE.txt"
+    "url": "/video-recording/static/js/2.5651cf6f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fdfae68ece04d7229dd6",
-    "url": "/video-recording/static/js/main.5823ce4d.chunk.js"
+    "revision": "ac2e369631f72cb76a8a",
+    "url": "/video-recording/static/js/main.c13b331c.chunk.js"
   },
   {
     "revision": "c03eb7f1587305ce3a67",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/video-recording/precache-manifest.e8140d0839c0c26e5111d7a27495ce6e.js"
+  "/video-recording/precache-manifest.2cd8ea6a6e5b4e1020dc0a6ec7aec85d.js"
 );
 
 self.addEventListener('message', (event) => {
